07976153379951a44301e0a484208d4d3f98bdf6
Mutudu <aeondxf@live.com>
2026-01-06 20:25:17 +0800

    dev

--- samples/Pico.DI.Sample/Program.cs
+++ samples/Pico.DI.Sample/Program.cs
@@ -1,29 +1,16 @@
 ﻿namespace Pico.DI.Sample;
 
-public static class ServiceConfig
+public static class Program
 {
-    public static void ConfigureServices(ISvcContainer container)
+    public static async Task Main(string[] args)
     {
-        // Note: ConfigureGeneratedServices() is now called automatically via Module Initializer
-        // when SvcContainer is created. Manual registration calls below are for descriptive purposes.
-        container
+        await using var container = new SvcContainer()
             .RegisterSingleton(typeof(ILogger<>), typeof(ConsoleLogger<>))
             .RegisterTransient<IGreeter, Greeter>()
             .RegisterScoped<GreetingService>();
-    }
-}
-
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        using var container = new SvcContainer();
-
-        // Configure services
-        ServiceConfig.ConfigureServices(container);
 
         // Create a scope and resolve services
-        using var scope = container.CreateScope();
+        await using var scope = container.CreateScope();
 
         var greetingService = scope.GetService<GreetingService>();
         greetingService.SayHello("World");
