07778d8b278224601c301669c624520c0bada7fe
Mutudu <aeondxf@live.com>
2026-01-08 14:45:19 +0800

    Remove unused code

--- src/Pico.DI/GlobalUsings.cs
+++ src/Pico.DI/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿// Global using directives
 
-global using System.Collections.Concurrent;
 global using System.Collections.Frozen;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Runtime.CompilerServices;
